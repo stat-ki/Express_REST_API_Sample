ccc1fbe3b1e193fd3150a7c261fdb3515e84bc5b
stat-ki <per_ik@outlook.com>
2020-06-07 13:35:16 +0000

    Create sample routes

---
+++ routes/samples.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  var param = {"値":"これはサンプルAPIです"};
+  res.header('Content-Type', 'application/json; charset=utf-8')
+  res.send(param);
+});
+
+router.get('/hello', function(req, res, next) {
+  var param = {"result":"Hello World !"};
+  res.header('Content-Type', 'application/json; charset=utf-8')
+  res.send(param);
+});
+
+module.exports = router;
